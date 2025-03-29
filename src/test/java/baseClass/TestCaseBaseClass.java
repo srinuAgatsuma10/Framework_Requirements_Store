05de0e4087f5bafb90056410ab924c17c7669887
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-29 07:35:08 +0530

    Extent Report Manager Update

---
+++ src/test/java/baseClass/TestCaseBaseClass.java
@@ -0,0 +1,33 @@
+package baseClass;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class TestCaseBaseClass {
+	WebDriver driver;
+	
+	
+	/*This method is connected to Extent Report manger for taking screenshots when test got failed.*/
+	// Capturing Screen Shot
+	public String captureScreen(String tname) throws IOException {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date(0));
+				
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		
+		String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+		File targetFile=new File(targetFilePath);
+		
+		sourceFile.renameTo(targetFile);
+			
+		return targetFilePath;
+
+	}
+}
